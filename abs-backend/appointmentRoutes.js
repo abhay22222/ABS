b359d4bfbaa7bd350afc5507a9590140063869a0
abhay <abhaybhongale@gmail.com>
2026-04-13 17:20:47 +0530

    new structure

--- abs-backend/appointmentRoutes.js
+++ abs-backend/appointmentRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const controller = require("../controllers/appointmentController");
+const controller = require("./controllers/appointmentController");
 
 
 router.post("/", controller.create);
--- abs-backend/server.js
+++ abs-backend/server.js
@@ -10,8 +10,7 @@ app.use(express.json());
 const appointmentRoutes = require("./routes/appointmentRoutes");
 app.use("/api/appointments", appointmentRoutes);
 
-const authRoutes = require("./routes/authRoutes");
-app.use("/api/auth", authRoutes);
+
 
 app.listen(5000, () => {
   console.log("🚀 Server running on http://localhost:5000");
